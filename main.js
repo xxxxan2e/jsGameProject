0beacbdf8f7162bbba2b15b3e213f73cc16a0148
xxxxan2e <xxxxan2e@gmail.com>
2023-12-08 23:06:53 +0900

    적군에게 총알이 명중하면 적군이 소멸. 스코어를 올라가게 하고 점수를 화면에 표시하기

--- main.js
+++ main.js
@@ -9,6 +9,9 @@ canvas.height=700;
 document.body.appendChild(canvas); // html에 canvas 세팅
 
 let galaxybackgroundImage,spaceshipImage,bulletImage,enemyImage,gameoverImage;
+let gameOver = false; // true이면 게임 끝남, false이면 게임 안 끝남
+let score = 0;
+
 
 // 우주선 좌표 (우주선은 움직이기 때문에 계속 바뀔 예정)
 let spaceshipX = canvas.width/2-32
@@ -22,12 +25,48 @@ function Bullet() {
     this.init=function() { // 초기화
         this.x = spaceshipX + 20;// 총알은 우주선에서 발사되어야 하기 때문에
         this.y = spaceshipY;
+        this.alive = true; // true이면 살아있는 총알, false면 죽은 총알
 
         bulletList.push(this);
     };
     this.update = function() {
         this.y -= 7;
     };
+
+    this.checkHit = function() {
+
+        for(let i=0; i < enemyList.length; i++) {
+            if(this.y <= enemyList[i].y && this.x >= enemyList[i].x && this.x<=enemyList[i].x+40 ){
+                score++;
+                this.alive = false; // 죽은 총알
+                enemyList.splice(i,1);
+            }
+        }
+        
+    };
+}
+
+function generateRandomValue(min,max) {
+    let randomNum = Math.floor(Math.random()*(max-min-1))+min
+    return randomNum;
+}
+
+let enemyList=[]
+function Enemy() {
+    this.x = 0;
+    this.y = 0;
+    this.init = function() {
+        this.y = 0 // 무조건 최상단 높이에서 시작
+        this.x = generateRandomValue(0,canvas.width-67) // x 좌표 랜덤 위치로 지정, canvas 범위 내로 한정해준다
+        enemyList.push(this);
+    };
+    this.update=function(){
+        this.y += 1; // 적군의 속도 조절
+
+        if(this.y >= canvas.height - 67) {
+            gameOver = true;
+        }
+    }
 }
 
 
@@ -69,7 +108,13 @@ function createBullet() {
     console.log("총알 생성!")
     let b = new Bullet() // 총알 하나 생성
     b.init()
-    console.log("새로운 총알 리스트",bulletList);
+}
+
+function createEnemy() {
+    const interval = setInterval(function() {
+        let e = new Enemy()
+        e.init()
+    },1000) // setInterval에는 2개의 매개변수 => (호출하고 싶은 함수, 호출 간격(시간)) 시간의 단위는 ms
 }
 
 
@@ -91,29 +136,51 @@ function update() {
 
     // 총알의 y좌표 업데이트하는 함수 호출
     for(let i=0; i<bulletList.length; i++) {
-        bulletList[i].update()
+        if (bulletList[i].alive) {
+            bulletList[i].update();
+        bulletList[i].checkHit();
+        }
+        
     };
+
+    for(let i=0; i<enemyList.length; i++) {
+        enemyList[i].update();
+    }
     
 }
 
 function render() { // 이미지를 불러오는 함수
     ctx.drawImage(galaxybackgroundImage, 0, 0, canvas.width, canvas.height); // 무슨 이미지를 어디에(x,y 좌표) 가로, 세로 크기
     ctx.drawImage(spaceshipImage,spaceshipX,spaceshipY);
+    ctx.fillText(`score:${score}`, 20, 20);
+    ctx.fillStyle = "white";
+    ctx.font = "20px Arial";
 
     for(let i = 0; i<bulletList.length;i++) {
-        ctx.drawImage(bulletImage,bulletList[i].x,bulletList[i].y)
+        if(bulletList[i].alive) {
+            ctx.drawImage(bulletImage,bulletList[i].x,bulletList[i].y);
+        }
+    }
+
+    for(let i=0; i<enemyList.length;i++) {
+        ctx.drawImage(enemyImage, enemyList[i].x, enemyList[i].y);
     }
 } 
 
 function main() {
-    update(); // 좌표값을 업데이트 하고
-    render(); // 그려주고
-    console.log("animation calls main function")
-    requestAnimationFrame(main); // 화면을 계속해서 호출해준다. 좌표값이 갱신되고 그 값을 렌더로 표현하고 애니메이션이 반복.
+    if (!gameOver) { // 적군이 바닥에 닿으면서 true로 결과값 바뀌고 main이 멈추게 된다
+        update(); // 좌표값을 업데이트 하고
+        render(); // 그려주고
+        console.log("animation calls main function")
+        requestAnimationFrame(main); // 화면을 계속해서 호출해준다. 좌표값이 갱신되고 그 값을 렌더로 표현하고 애니메이션이 반복.
+    } else {
+        ctx.drawImage(gameoverImage,10,100,380,380);
+    }
 }
 
 loadImage();
 setupKeyboardListener() 
+createEnemy();
 main();
 
 // 방향키를 누르면
@@ -126,3 +193,16 @@ main();
 // 3. 발사된 총알들은 총알 배열(Array)에 저장함
 // 4. 모든 총알들은 x,y 좌표 값이 있어야 함
 // 5. 총알 배열을 가지고 render 그려줌 
+
+// 적군 만들기
+// x,y, init, update
+// 적군은 위치 랜덤
+// 적군은 밑으로 내려옴 = y좌표가 증가한다
+// 1초마다 하나씩 적군이 생성
+// 적군의 우주선이 바닥에 닿으면 game over
+// 적군과 총알이 만나면 적군 처치. score 1점 획득
+
+// 적군이 죽는다 = 총알이 적군에게 닿는다
+// 총알의 y좌표 <= 적군의 y좌표 AND 총알의 x좌표 >= 적군의 x좌표 AND 총알의 x 좌표 <= 적군의 x좌표 + 적군의 넓이
+// => 닿았다
+// => 총알이 죽게 됨, 적군의 우주선이 없어짐, 점수 확득!
